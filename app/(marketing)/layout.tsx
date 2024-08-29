34198b49339bdeb1f2be37acda204a9b04911b75
zhaleh <zhalehelmi@gmail.com>
2024-08-29 12:15:11 +0330

    add navbar

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -1,7 +1,10 @@
+import { Navbar } from "./_components/navbar";
+
 const MarketingLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="h-full bg-slate-100">
-      {/* navbar */}
+      <Navbar />
+
       <main className="pt-40 pb-20 bg-slate-100">{children}</main>
       {/* footer */}
     </div>
